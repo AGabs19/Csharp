b412d11100c551b343b332b47e2df9909f7d444f
AGabs <anagabriella@infosiscs.com>
2022-07-22 11:49:36 -0300

    Criei a classe posicao

--- XadrezConsole/XadrezConsole/Program.cs
+++ XadrezConsole/XadrezConsole/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tabuleiro;
+using System;
+
+namespace XadrezConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao P;
+            P = new Posicao(3, 4); //Instanciei nova posição P
+
+            Console.WriteLine("Posição: " + P);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ XadrezConsole/XadrezConsole/Tabuleiro/Posicao.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Tabuleiro
+{
+    class Posicao
+    {
+        //Atributos
+        public int linha { get; set; }
+        public int coluna { get; set; }
+
+        //Construtor
+        public Posicao (int linha, int coluna)
+        {
+            this.linha = linha;
+            this.coluna = coluna;
+        }
+
+        public override string ToString()
+        {
+            return linha
+                + ","
+                + coluna;
+        }
+    }
+}
